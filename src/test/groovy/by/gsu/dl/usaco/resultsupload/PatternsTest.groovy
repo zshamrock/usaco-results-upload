488bddb7885cae54ac6f9d14354aba667c5c208c
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2015-03-07 16:45:29 +0100

    Extra test for Patterns.

--- src/test/groovy/by/gsu/dl/usaco/resultsupload/PatternsTest.groovy
+++ src/test/groovy/by/gsu/dl/usaco/resultsupload/PatternsTest.groovy
@@ -1,14 +1,25 @@
 package by.gsu.dl.usaco.resultsupload
 
+import by.gsu.dl.usaco.resultsupload.domain.Division
 import by.gsu.dl.usaco.resultsupload.exception.NoMatchesException
 import spock.lang.Specification
 
 class PatternsTest extends Specification {
     def "throw no matches exception"() {
         when:
-        Patterns.matchesContest("Final Results: USACO 2014 Contest, Bronze");
+        Patterns.matchesContest("Final Results: USACO 2014 Contest, Bronze")
 
         then:
         thrown(NoMatchesException)
+    }
+
+    def "matches contest"() {
+        when:
+        def contest = Patterns.matchesContest("Final Results: USACO 2015 January Contest, Gold")
+
+        then:
+        contest.year == 2015
+        contest.month == "January"
+        contest.division == Division.GOLD
     }
 }
